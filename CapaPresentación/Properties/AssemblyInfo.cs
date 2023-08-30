3a20a5dd791d6e98c78ae10a4433bee75ce35ade
Nicolas Miracco <ext_Nicolas.Miracco@globalprocessing.com.ar>
2023-08-29 22:31:58 -0300

    Definiciones para generar instalador

--- CapaPresentación/Properties/AssemblyInfo.cs
+++ CapaPresentación/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("CapaPresentación")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Taller Mecánico")]
+[assembly: AssemblyDescription("Gestor de un taller mecánico")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CapaPresentación")]
+[assembly: AssemblyCompany("Nicolás Manuel Miracco")]
+[assembly: AssemblyProduct("Taller Mecánico")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
 // utilizando el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
